058633b42b0f2d2cef4fb1b8f91f40765bc9ee58
rslivepark <rslivepark@gmail.com>
2024-03-19 22:20:24 +0900

    스터디 2회차 과제

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import './App.css';
+import Box from './component/Box';
+
+// 1. 박스 2개(타이틀, 사진, 결과)
+// 2. 가위 바위 보 버튼이 있다
+// 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
+// 4. 컴퓨터는 랜덤하게 아이템 선택이 된다
+// 5. 3,4의 결과를 가지고 누가 이겼는지 승패를 따진다
+// 6. 승패의 결과에 따라 테두리 색이 바뀐다.(이기면 초록, 지면 빨강, 비기면 검은색)
+
+const choice = {
+  rock: {
+    name: 'Rock',
+    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FpSJwo%2FbtqXJV1lACE%2Fnx5XrxkCLWXh9UsnoS8vbK%2Fimg.png',
+  },
+  scissor: {
+    name: 'Scissor',
+    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FHfURw%2FbtqXKvOTNWK%2FgWTwPXEg9QzSV0ilOuwuak%2Fimg.png',
+  },
+  paper: {
+    name: 'Paper',
+    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbmjB2s%2FbtqXHhp6kpG%2FTH14W4U612SxKo9uuR2sB0%2Fimg.png',
+  },
+};
+
+function App() {
+  let [userSelect, setUserSelect] = useState(null);
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+  };
+  return (
+    <div>
+      <div className='main'>
+        <Box title='you' item={userSelect}></Box>
+        <Box title='computer'></Box>
+      </div>
+      <div className='main'>
+        <button onClick={() => play('scissros')}>가위</button>
+        <button onClick={() => play('rock')}>바위</button>
+        <button onClick={() => play('paper')}>보</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
