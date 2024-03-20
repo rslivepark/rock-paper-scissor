455ad0a8c4e5135443472b3abab0cd64c35c9caa
박은선 <146307227+rslivepark@users.noreply.github.com>
2024-03-20 20:21:26 +0900

    스터디 2회차 과제
    가위바위보 게임 - 이기고 지는 표시

--- src/App.js
+++ src/App.js
@@ -10,39 +10,84 @@ import Box from './component/Box';
 // 6. 승패의 결과에 따라 테두리 색이 바뀐다.(이기면 초록, 지면 빨강, 비기면 검은색)
 
 const choice = {
-  rock: {
-    name: 'Rock',
-    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FpSJwo%2FbtqXJV1lACE%2Fnx5XrxkCLWXh9UsnoS8vbK%2Fimg.png',
-  },
-  scissor: {
-    name: 'Scissor',
-    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FHfURw%2FbtqXKvOTNWK%2FgWTwPXEg9QzSV0ilOuwuak%2Fimg.png',
-  },
-  paper: {
-    name: 'Paper',
-    img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbmjB2s%2FbtqXHhp6kpG%2FTH14W4U612SxKo9uuR2sB0%2Fimg.png',
-  },
+	rock: {
+		name: 'Rock',
+		img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FpSJwo%2FbtqXJV1lACE%2Fnx5XrxkCLWXh9UsnoS8vbK%2Fimg.png',
+	},
+	scissor: {
+		name: 'Scissor',
+		img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FHfURw%2FbtqXKvOTNWK%2FgWTwPXEg9QzSV0ilOuwuak%2Fimg.png',
+	},
+	paper: {
+		name: 'Paper',
+		img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbmjB2s%2FbtqXHhp6kpG%2FTH14W4U612SxKo9uuR2sB0%2Fimg.png',
+	},
 };
 
 function App() {
-  let [userSelect, setUserSelect] = useState(null);
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
 
-  const play = (userChoice) => {
+	const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice)
+    setResult(judgement(choice[userChoice], computerChoice));
   };
-  return (
-    <div>
-      <div className='main'>
-        <Box title='you' item={userSelect}></Box>
-        <Box title='computer'></Box>
-      </div>
-      <div className='main'>
-        <button onClick={() => play('scissor')}>가위</button>
-        <button onClick={() => play('rock')}>바위</button>
-        <button onClick={() => play('paper')}>보</button>
-      </div>
-    </div>
-  );
+
+  const judgement = (user, computer) => {
+		/* 게임 로직
+        user == computer >> tie 
+        user == rock, computer == scissor user 승
+        user == rock, computer == paper user 짐
+        user == scissor, computer == paper user 승
+        user == scissor, computer == rock user 짐
+        user == paper, computer == rock user 승
+        user == paper, computer == scissor user 짐
+        */
+    
+    if (user.name === computer.name) return 'tie';
+		if (
+			(user.name === 'Rock' && computer.name === 'Scissor') ||
+			(user.name === 'Scissor' && computer.name === 'Paper') ||
+			(user.name === 'Paper' && computer.name === 'Rock')
+		) {
+			return 'win';
+		} else {
+			return 'lose';
+		}
+
+  } // if끝
+
+  
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); // 객체에 키값만 뽑아서 array로 만들어주는 함수
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem]
+    return choice[final];
+  }
+
+
+
+	return (
+		<div>
+			<div className="main">
+				<Box title="you" item={userSelect} result={result}></Box>
+				{/* <Box title="computer" item={computerSelect} result={result}></Box> */}
+				<Box
+					title="Computer"
+					item={computerSelect}
+					result={result === 'win' ? 'lose' : result === 'lose' ? 'win' : 'tie'}
+				></Box>
+			</div>
+			<div className="main">
+				<button onClick={() => play('scissor')}>가위</button>
+				<button onClick={() => play('rock')}>바위</button>
+				<button onClick={() => play('paper')}>보</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
+
+
+
 const Box = (props) => {
-  console.log('props', props);
-  return (
-    <div className='box'>
-      <h1>{props.title}</h1>
-      <img className='item-img' src={props.item && props.item.img} alt='img'></img>
-      <h2>Win</h2>
-    </div>
-  );
+	const resultClassName = () => {
+		if (props.result) {
+			switch (props.result) {
+				case 'win':
+					return 'border-win';
+				case 'lose':
+					return 'border-lose';
+				case 'tie':
+					return 'border-tie';
+				default:
+					return '';
+			}
+		}
+		return ''; // 결과가 없을 때는 테두리 클래스를 적용하지 않습니다.
+	};
+	return (
+		<div className={`box ${resultClassName()}`}>
+			<h1>{props.title}</h1>
+			<img
+				className="item-img"
+				src={props.item && props.item.img}
+				alt="가위바위보"
+			></img>
+			<h2>{props.result}</h2>
+		</div>
+	);
 };
 
 export default Box;
